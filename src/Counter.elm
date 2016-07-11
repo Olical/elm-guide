3915a410164dbf78d0a41a28a8d3299168a30c0e
Oliver Caldwell <olliec87@gmail.com>
2016-07-11 07:28:30 +0100

    Add a reset button to the couter

--- src/Counter.elm
+++ src/Counter.elm
@@ -5,7 +5,7 @@ import Html.Events exposing (onClick)
 main =
   Html.beginnerProgram { model = 0, view = view, update = update }
 
-type Msg = Increment | Decrement
+type Msg = Increment | Decrement | Reset
 
 update msg model =
   case msg of
@@ -15,9 +15,13 @@ update msg model =
     Decrement ->
       model - 1
 
+    Reset ->
+      0
+
 view model =
   div []
     [ button [ onClick Decrement ] [ text "-" ]
     , div [] [ text (toString model) ]
     , button [ onClick Increment ] [ text "+" ]
+    , button [ onClick Reset ] [ text "reset" ]
     ]
